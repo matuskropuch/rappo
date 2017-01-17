8158c25e5472766d26c668f07babfc102a93ab6d
Matus Kropuch <kropuchm@gmail.com>
2017-01-17 19:16:00 +0000

    removed unnecessary things

--- resources/views/rappers/index.blade.php
+++ resources/views/rappers/index.blade.php
@@ -5,26 +5,24 @@
 <h1>Rappers</h1>
 
 <div class="panel panel-default">
-    <div class="panel-body">
-        <table class="table table-stripped">
-            <thead>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Nickname</th>
-                <th>Date of birth</th>
-            </thead>
-            <tbody>
-                @foreach($rappers as $rapper)
-                <tr>
-                    <td><a href="{{ route('rappers.show', $rapper->nickname) }}">{{ $rapper->first_name }}</a></td>
-                    <td>{{ $rapper->last_name }}</td>
-                    <td>{{ $rapper->nickname }}</td>
-                    <td>{{ $rapper->born_at }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+    <table class="table table-stripped">
+        <thead>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Nickname</th>
+            <th>Date of birth</th>
+        </thead>
+        <tbody>
+            @foreach($rappers as $rapper)
+            <tr>
+                <td><a href="{{ route('rappers.show', $rapper->nickname) }}">{{ $rapper->first_name }}</a></td>
+                <td>{{ $rapper->last_name }}</td>
+                <td>{{ $rapper->nickname }}</td>
+                <td>{{ $rapper->born_at }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
 
 <a href="{{ route('rappers.create') }}" class="btn btn-primary" role="button">Add a Rapper</a>
