a3a7367799babb1914f972bf3ee77a87ee56d77a
Matus Kropuch <kropuchm@gmail.com>
2017-02-05 12:18:57 +0000

    made requested minor changes

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,6 +1,3 @@
-
-window._ = require('lodash');
-
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
@@ -10,27 +7,6 @@ window._ = require('lodash');
 window.$ = window.jQuery = require('jquery');
 
 require('bootstrap-sass');
-
-/**
- * Vue is a modern JavaScript library for building interactive web interfaces
- * using reactive data binding and reusable components. Vue's API is clean
- * and simple, leaving you to focus on building your next great project.
- */
-
-window.Vue = require('vue');
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = require('axios');
-
-window.axios.defaults.headers.common = {
-    'X-CSRF-TOKEN': window.Laravel.csrfToken,
-    'X-Requested-With': 'XMLHttpRequest'
-};
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,5 @@ const { mix } = require('laravel-mix');
 mix.combine([
     'node_modules/jquery/dist/jquery.min.js',
     'node_modules/bootstrap/dist/js/bootstrap.min.js'
-], 'public/js/app.js')
-   .combine(['node_modules/bootstrap/dist/css/bootstrap.min.css'], 'public/css/app.css')
+    ], 'public/js/app.js')
+   .combine(['node_modules/bootstrap/dist/css/bootstrap.min.css'], 'public/css/app.css');
